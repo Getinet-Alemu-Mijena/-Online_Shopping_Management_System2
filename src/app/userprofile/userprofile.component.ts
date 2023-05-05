f0390939093482528d5778564e6f47d3fbc15322
Getinet Alemu Mijena <121330349+Getinet-Alemu-Mijena@users.noreply.github.com>
2023-05-06 02:51:57 +0300

    Update of user profile

--- src/app/userprofile/userprofile.component.ts
+++ src/app/userprofile/userprofile.component.ts
@@ -6,13 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./userprofile.component.css']
 })
 export class UserprofileComponent {
+
+
   isNavbarOpen:boolean = false;
+  edit_profile:boolean = false;
   action:boolean = false;
+
+  constructor(){}
   toggleNavbar():boolean {
     alert("clicked");
    return this.isNavbarOpen = !this.isNavbarOpen;
   }
   actionOn(){
      this.action = true;
+  }
+
+  editProfile(){
+     this.edit_profile = !this.edit_profile;
   }
 }
