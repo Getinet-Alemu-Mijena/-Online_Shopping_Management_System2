9834530ac2958565011df5b585462da9718d03dc
Getinet Alemu Mijena <121330349+Getinet-Alemu-Mijena@users.noreply.github.com>
2023-07-15 19:54:48 +0300

    update on seller, customer, admin and registration page

--- src/app/seller/seller.component.ts
+++ src/app/seller/seller.component.ts
@@ -6,24 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./seller.component.css']
 })
 export class SellerComponent {
+
   isNavbarOpen:boolean = false;
   action:boolean = false;
   uploadproduct:boolean = false;
   viewproduct:boolean = false;
+  manageproduct:boolean = false;
+
   toggleNavbar():boolean {
     alert("clicked");
    return this.isNavbarOpen = !this.isNavbarOpen;
   }
+
   actionOn(){
      this.action = true;
   }
+
   uploadProduct(){
     this.uploadproduct = true;
     this.viewproduct = false;
+    this.manageproduct = false;
   }
+  
   viewProduct(){
     this.viewproduct = true;
     this.uploadproduct = false;
+    this.manageproduct = false;
+  }
+
+  manageProduct(){
+   this.manageproduct = true;
+   this.uploadproduct = false;
+   this.viewproduct = false;
   }
 
 }
